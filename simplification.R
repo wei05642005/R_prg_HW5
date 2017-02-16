5ff3957d437e8a5546966d1724d8d02d538e97f2
wei05642005 <lathy4045@gmail.com>
2017-02-16 22:50:40 +0800

    Add files via upload

--- simplification.R
+++ simplification.R
@@ -1,4 +1,11 @@
+load(url("https://storage.googleapis.com/r_rookies/straw_hat_df.RData"))
+this_year <- as.numeric(format(Sys.Date(), '%Y'))
+birth_year <- this_year - straw_hat_df$age
+birth_date_chr <- paste(birth_year, straw_hat_df$birthday, sep = "-")
+straw_hat_df$birth_date <- as.Date(birth_date_chr)
+
 #Simplify the original code
+load(url("https://storage.googleapis.com/r_rookies/straw_hat_df.RData"))
 birth_year <- Sys.Date() %>%
   format(format = "%Y") %>%
   as.numeric() %>%
